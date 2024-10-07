075284791cc40d62bceea17141d0b7adef489233
Alan Clucas <alan@clucas.org>
2024-10-07 08:35:46 +0100

    wip
    Signed-off-by: Alan Clucas <alan@clucas.org>

--- hack/test-examples.sh
+++ hack/test-examples.sh
@@ -1,41 +1,44 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
+parallel=10
 # Load the configmaps that contains the parameter values used for certain examples.
 kubectl apply -f examples/configmaps/simple-parameters-configmap.yaml
-# testworkflows=("start")
-# #grep -lR 'workflows.argoproj.io/test' examples/* | while read f ; do
-# grep -vlR 'workflows.argoproj.io/no-test' examples/*.yaml | while read -r f; do
-# 	echo $f
-# 	testworkflows+=("$f")
-# 	echo "${testworkflows[@]}"
-# done
 
-
-testworkflows=($(grep -vlR 'workflows.argoproj.io/no-test' examples/*.yaml | tr '\n' ' '))
+testworkflows=($(grep -LR 'workflows.argoproj.io/do-not-test' examples/*.yaml | tr '\n' ' '))
 # If arguments, test those files instead
 if [ $# -gt 0 ]; then
    testworkflows=("$@")
 fi
 
-echo "Checking for banned images..."
-for f in "${testworkflows[@]}"; do
-  echo " - $f"
-  test 0 == "$(grep -o 'image: .*' "$f" | grep -cv 'argoproj/argosay:v2\|python:alpine3.6\|busybox')"
-done
-
+exitcode=0
+# echo "Checking for banned images..."
+# for f in "${testworkflows[@]}"; do
+#   echo " - $f"
+#   if [ 0 != "$(grep -o 'image: .*' "$f" | grep -cv 'argoproj/argosay:v2\|python:alpine3.6\|busybox\|alpine:latest\|argoproj/argoexec:latest')" ]; then
+# 	  echo "BANNED image in $f"
+# 	  exitcode=1
+#   fi
+# done
+# exit $exitcode
 #trap 'kubectl get wf' EXIT
 
-for f in "${testworkflows[@]}"; do
-  echo "Running $f..."
-  name=$(kubectl create -f "$f" -o name)
+# for f in "${testworkflows[@]}"; do
+#   echo "Running $f..."
+#   name=$(kubectl create -f "$f" -o name)
 
-  echo "Waiting for completion of $f..."
-  kubectl wait --for=condition=Completed "$name"
-  phase="$(kubectl get "$name" -o 'jsonpath={.status.phase}')"
-  echo " -> $phase"
-  test Succeeded == "$phase"
+#   echo "Waiting for completion of $f..."
+#   kubectl wait --timeout=300s --for=condition=Completed "$name"
+#   phase="$(kubectl get "$name" -o 'jsonpath={.status.phase}')"
+#   echo " -> $phase"
+#   if [ Succeeded != "$phase" ]; then
+# 	  echo "Test $name in phase $phase"
+# 	  exitcode=1
+#   fi
 
-  echo "Deleting $f..."
-  kubectl delete "$name"
-done
+#   echo "Deleting $f..."
+#   kubectl delete "$name"
+# done
+printf "%s\0" "${testworkflows[@]}" | xargs -0 -I {} -P $parallel hack/test-one-example.sh {}
+
+exit $exitcode
