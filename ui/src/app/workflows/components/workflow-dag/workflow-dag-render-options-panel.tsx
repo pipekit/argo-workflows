efb599fce97e341c1e3fcbeec38fc0c683dd52c9
Adrien Delannoy <a.delannoyfr@gmail.com>
2024-10-30 12:03:52 +0100

    feat(ui): Group nodes based on templateRef and show invoking template name
    Signed-off-by: Adrien Delannoy <a.delannoyfr@gmail.com>

--- ui/src/app/workflows/components/workflow-dag/workflow-dag-render-options-panel.tsx
+++ ui/src/app/workflows/components/workflow-dag/workflow-dag-render-options-panel.tsx
@@ -40,6 +40,28 @@ export class WorkflowDagRenderOptionsPanel extends React.Component<WorkflowDagRe
                     title='Expand all nodes'>
                     <i className='fa fa-expand fa-fw' data-fa-transform='rotate-45' />
                 </a>
+                <a
+                    onClick={() =>
+                        this.props.onChange({
+                            ...this.workflowDagRenderOptions,
+                            showTemplateRefsGrouping: !this.workflowDagRenderOptions.showTemplateRefsGrouping
+                        })
+                    }
+                    className={this.workflowDagRenderOptions.showTemplateRefsGrouping ? 'active' : ''}
+                    title='Group by templateRefs'>
+                    <i className='fa fa-sitemap fa-fw' />
+                </a>
+                <a
+                    onClick={() =>
+                        this.props.onChange({
+                            ...this.workflowDagRenderOptions,
+                            showInvokingTemplateName: !this.workflowDagRenderOptions.showInvokingTemplateName
+                        })
+                    }
+                    className={this.workflowDagRenderOptions.showInvokingTemplateName ? 'active' : ''}
+                    title='Show invoking template name'>
+                    <i className='fa fa-tag fa-fw' data-fa-transform='rotate-45' />
+                </a>
             </>
         );
     }
