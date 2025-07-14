14da4b05006e4bbe4c888075cefb6557fc27dd5a
Alan Clucas <alan@clucas.org>
2025-07-14 11:18:08 +0100

    feat: remove logrus and propogate context everywhere
    Signed-off-by: Alan Clucas <alan@clucas.org>

--- persist/sqldb/workflow_archive.go
+++ persist/sqldb/workflow_archive.go
@@ -101,9 +101,8 @@ func NewWorkflowArchive(session db.Session, clusterName, managedNamespace string
 }
 
 func (r *workflowArchive) ArchiveWorkflow(ctx context.Context, wf *wfv1.Workflow) error {
-	logger := logging.GetLoggerFromContext(ctx)
-	logCtx := logger.WithFields(logging.Fields{"uid": wf.UID, "labels": wf.GetLabels()})
-	logCtx.Debug(ctx, "Archiving workflow")
+	logger := logging.RequireLoggerFromContext(ctx).WithFields(logging.Fields{"uid": wf.UID, "labels": wf.GetLabels()})
+	logger.Debug(ctx, "Archiving workflow")
 	wf.Labels[common.LabelKeyWorkflowArchivingStatus] = "Persisted"
 	workflow, err := json.Marshal(wf)
 	if err != nil {
@@ -456,7 +455,7 @@ func (r *workflowArchive) GetWorkflowForEstimator(ctx context.Context, namespace
 }
 
 func (r *workflowArchive) DeleteWorkflow(ctx context.Context, uid string) error {
-	logger := logging.GetLoggerFromContext(ctx)
+	logger := logging.RequireLoggerFromContext(ctx)
 	rs, err := r.session.SQL().
 		DeleteFrom(archiveTableName).
 		Where(r.clusterManagedNamespaceAndInstanceID()).
@@ -474,7 +473,7 @@ func (r *workflowArchive) DeleteWorkflow(ctx context.Context, uid string) error 
 }
 
 func (r *workflowArchive) DeleteExpiredWorkflows(ctx context.Context, ttl time.Duration) error {
-	logger := logging.GetLoggerFromContext(ctx)
+	logger := logging.RequireLoggerFromContext(ctx)
 	rs, err := r.session.SQL().
 		DeleteFrom(archiveTableName).
 		Where(r.clusterManagedNamespaceAndInstanceID()).
