f7f3c7b3a64038c27998113b7a71b6fe0c5d3bb6
J.P. Zivalich <j.p.zivalich@gmail.com>
2025-07-30 17:38:32 +0200

    use require instead of assert
    Signed-off-by: J.P. Zivalich <j.p.zivalich@gmail.com>

--- util/env/env_test.go
+++ util/env/env_test.go
@@ -5,50 +5,50 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/argoproj/argo-workflows/v3/util/logging"
 )
 
 func TestLookupEnvDurationOr(t *testing.T) {
 	ctx := logging.TestContext(t.Context())
-	assert.Equal(t, time.Second, LookupEnvDurationOr(ctx, "", time.Second), "default value")
+	require.Equal(t, time.Second, LookupEnvDurationOr(ctx, "", time.Second), "default value")
 	t.Setenv("FOO", "bar")
-	assert.Panics(t, func() { LookupEnvDurationOr(ctx, "FOO", time.Second) }, "bad value")
+	require.Panics(t, func() { LookupEnvDurationOr(ctx, "FOO", time.Second) }, "bad value")
 	t.Setenv("FOO", "1h")
-	assert.Equal(t, time.Hour, LookupEnvDurationOr(ctx, "FOO", time.Second), "env var value")
+	require.Equal(t, time.Hour, LookupEnvDurationOr(ctx, "FOO", time.Second), "env var value")
 	t.Setenv("FOO", "")
-	assert.Equal(t, time.Second, LookupEnvDurationOr(ctx, "FOO", time.Second), "empty var value; default value")
+	require.Equal(t, time.Second, LookupEnvDurationOr(ctx, "FOO", time.Second), "empty var value; default value")
 }
 
 func TestLookupEnvIntOr(t *testing.T) {
 	ctx := logging.TestContext(t.Context())
-	assert.Equal(t, 1, LookupEnvIntOr(ctx, "", 1), "default value")
+	require.Equal(t, 1, LookupEnvIntOr(ctx, "", 1), "default value")
 	t.Setenv("FOO", "not-int")
-	assert.Panics(t, func() { LookupEnvIntOr(ctx, "FOO", 1) }, "bad value")
+	require.Panics(t, func() { LookupEnvIntOr(ctx, "FOO", 1) }, "bad value")
 	t.Setenv("FOO", "2")
-	assert.Equal(t, 2, LookupEnvIntOr(ctx, "FOO", 1), "env var value")
+	require.Equal(t, 2, LookupEnvIntOr(ctx, "FOO", 1), "env var value")
 	t.Setenv("FOO", "")
-	assert.Equal(t, 1, LookupEnvIntOr(ctx, "FOO", 1), "empty var value; default value")
+	require.Equal(t, 1, LookupEnvIntOr(ctx, "FOO", 1), "empty var value; default value")
 }
 
 func TestLookupEnvFloatOr(t *testing.T) {
 	ctx := logging.TestContext(t.Context())
-	assert.InEpsilon(t, 1., LookupEnvFloatOr(ctx, "", 1.), 0.001, "default value")
+	require.InEpsilon(t, 1., LookupEnvFloatOr(ctx, "", 1.), 0.001, "default value")
 	t.Setenv("FOO", "not-float")
-	assert.Panics(t, func() { LookupEnvFloatOr(ctx, "FOO", 1.) }, "bad value")
+	require.Panics(t, func() { LookupEnvFloatOr(ctx, "FOO", 1.) }, "bad value")
 	t.Setenv("FOO", "2.0")
-	assert.InEpsilon(t, 2., LookupEnvFloatOr(ctx, "FOO", 1.), 0.001, "env var value")
+	require.InEpsilon(t, 2., LookupEnvFloatOr(ctx, "FOO", 1.), 0.001, "env var value")
 	t.Setenv("FOO", "")
-	assert.InEpsilon(t, 1., LookupEnvFloatOr(ctx, "FOO", 1.), 0.001, "empty var value; default value")
+	require.InEpsilon(t, 1., LookupEnvFloatOr(ctx, "FOO", 1.), 0.001, "empty var value; default value")
 }
 
 func TestLookupEnvStringOr(t *testing.T) {
-	assert.Equal(t, "a", LookupEnvStringOr("", "a"), "default value")
+	require.Equal(t, "a", LookupEnvStringOr("", "a"), "default value")
 	t.Setenv("FOO", "b")
-	assert.Equal(t, "b", LookupEnvStringOr("FOO", "a"), "env var value")
+	require.Equal(t, "b", LookupEnvStringOr("FOO", "a"), "env var value")
 	t.Setenv("FOO", "")
-	assert.Equal(t, "a", LookupEnvStringOr("FOO", "a"), "empty var value; default value")
+	require.Equal(t, "a", LookupEnvStringOr("FOO", "a"), "empty var value; default value")
 }
 
 func TestGetHostname(t *testing.T) {
@@ -57,9 +57,9 @@ func TestGetHostname(t *testing.T) {
 	defer os.Setenv("HOSTNAME", originalHostname)
 
 	os.Setenv("HOSTNAME", "test-hostname")
-	assert.Equal(t, "test-hostname", GetHostname())
+	require.Equal(t, "test-hostname", GetHostname())
 
 	// Test when HOSTNAME is not set
 	os.Unsetenv("HOSTNAME")
-	assert.Equal(t, "", GetHostname())
+	require.Empty(t, GetHostname())
 }
